3ef5c74e20dfb3c3b4f1b008c37c0a3a9bb4cf58
AM-ML <aliredamoumneh@gmail.com>
2024-05-24 06:03:11 +0300

    Added get_random_int pseudo random function to library.

--- lib.c
+++ lib.c
@@ -13,3 +13,10 @@ int read_line(char str[], int n)
 
 	return s-str;
 }
+
+int get_random_int(int lower_range, int upper_range)
+{
+	srandom((unsigned int) time(NULL));
+
+	return random() % upper_range+1 + lower_range; // returns random int in range of a -> b
+}
