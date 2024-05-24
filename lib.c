5e812836c7d39ecf9979a0a3c2ec4674d433f52c
AM-ML <aliredamoumneh@gmail.com>
2024-05-24 14:14:07 +0300

    Lib.c library refactoring.

--- lib.c
+++ lib.c
@@ -21,22 +21,6 @@ int get_random_int(int lower_range, int upper_range)
 	return random() % upper_range+1 + lower_range; // returns random int in range of a -> b
 }
 
-// char* strLower(char *str)
-// {
-//     int len = strlen(str);
-//     char *s = malloc(len + 1); // Allocate memory for the new string
-
-//     if (s == NULL) // Check if memory allocation was successful
-//         return NULL;
-
-//     strcpy(s, str);
-
-//     for (int i = 0; i < len; i++) {
-//         s[i] = tolower((unsigned char) s[i]); // Use tolower for better readability
-//     }
-
-//     return s;
-// }
 
 char* strLower(const char *str)
 {
