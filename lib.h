2788fc4a974a047aaf45f2d0f05c222ef6e9921d
AM-ML <aliredamoumneh@gmail.com>
2024-05-24 14:10:06 +0300

    String Functions: StrLower StrUpper.

--- lib.h
+++ lib.h
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int read_line(char str[], int n);
 int get_random_int(int lower_range, int upper_range);
-
+char* strLower(const char *str);
+char* strUpper(const char *str);
