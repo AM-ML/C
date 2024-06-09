a4f63c979d9b022ef214481dcecd0989be56fef8
AM-ML <aliredamoumneh@gmail.com>
2024-06-09 17:42:35 +0300

    Lib.c library refactoring.

--- lib.h
+++ lib.h
@@ -11,35 +11,17 @@
 #define GREEN "\033[1;92m"
 #define YELLOW "\033[1;93m"
 
-#define PRINT_INT(n) printf("\033[1;92m::: " #n " \033[0;34m= \033[1;96m%d\n\033[0;0m", n)
-
 typedef char * string;
 
-static inline void seed_random();
 int read_line(char str[], int n);
 int get_random_int(int lower_range, int upper_range);
 char* strLower(const char *str);
 char* strUpper(const char *str);
-char* capitalize(const char *str);
+char *capitalize(const char *str);
 size_t Max_String_Length(char *strings[], size_t size);
 
-
-static inline void cyan() {
-    printf("\033[1;96m");
-}
-
-static inline void green() {
-    printf("\033[1;92m");
-}
-
-static inline void red() {
-    printf("\033[1;91m");
-}
-
-static inline void yellow() {
-    printf("\033[1;93m");
-}
-
-static inline void reset() {
-    printf("\033[0;0m");
-}
+void cyan();
+void green();
+void red();
+void yellow();
+void reset();
