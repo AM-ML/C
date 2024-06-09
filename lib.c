6cb9c585189b9d7cf9d55dd6ed88050d19621e23
AM-ML <aliredamoumneh@gmail.com>
2024-06-09 17:31:40 +0300

    lib refactoring: making lib.h faster to compile.

--- lib.c
+++ lib.c
@@ -1,102 +1,91 @@
 #include "lib.h"
 
-int read_line(char str[], int n)
-{
-	char *s = str;
-	int c;
-
-	while((c = getchar()) != '\n')
-	{
-		if(s-str < n)
-			*s++ = (char) c;
-	}
-	*s = '\0';
-
-	return (int) (s-str);
+// Seeding random number generator only once
+static inline void seed_random() {
+    static int seeded = 0;
+    if (!seeded) {
+        srandom((unsigned int) time(NULL));
+        seeded = 1;
+    }
 }
 
-int get_random_int(int lower_range, int upper_range)
-{
-	srandom((unsigned int) time(NULL));
+int read_line(char str[], int n) {
+    char *s = str;
+    int c;
 
-	return (int) (random() % upper_range+1 + lower_range); // returns random int in range of a -> b
+    while ((c = getchar()) != '\n' && s - str < n) {
+        *s++ = (char) c;
+    }
+    *s = '\0';
+
+    return (int)(s - str);
 }
 
-
-char* strLower(const char *str)
-{
-	size_t len = strlen(str);
-	char *s = malloc(len+1);
-
-	if(s == NULL)
-		return NULL;
-
-	strcpy(s, str);
-
-	for(int i = 0 ; i < len; i ++)
-		if(s[i] < 91)
-			s[i] += 32;
-
-	return s;
+int get_random_int(int lower_range, int upper_range) {
+    seed_random();
+    return (int)(random() % (upper_range - lower_range + 1) + lower_range); // returns random int in range of a -> b
 }
 
-char* strUpper(const char *str)
-{
-	size_t len = strlen(str);
-	char *s = malloc(len+1);
+char* strLower(const char *str) {
+    size_t len = strlen(str);
+    char *s = malloc(len + 1);
 
-	if(s == NULL)
-		return NULL;
+    if (s == NULL) {
+        return NULL;
+    }
 
-	strcpy(s, str);
+    for (size_t i = 0; i < len; i++) {
+        s[i] = (str[i] >= 'A' && str[i] <= 'Z') ? str[i] + 32 : str[i];
+    }
+    s[len] = '\0';
 
-	for(int i = 0 ; i < len; i ++)
-		if(s[i] > 96)
-			s[i] -= 32;
-
-	return s;
+    return s;
 }
 
-char *capitalize(const char *str)
-{
-	char *s = malloc(strlen(str) + 1);
+char* strUpper(const char *str) {
+    size_t len = strlen(str);
+    char *s = malloc(len + 1);
 
-	strcpy(s, str);
+    if (s == NULL) {
+        return NULL;
+    }
 
-	if(s[0] > 96)
-		s[0] -= 32;
+    for (size_t i = 0; i < len; i++) {
+        s[i] = (str[i] >= 'a' && str[i] <= 'z') ? str[i] - 32 : str[i];
+    }
+    s[len] = '\0';
 
-	return s;
+    return s;
+}
+
+char* capitalize(const char *str) {
+    size_t len = strlen(str);
+    char *s = malloc(len + 1);
+
+    if (s == NULL) {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < len; i++) {
+        s[i] = str[i];
+    }
+
+    if (s[0] >= 'a' && s[0] <= 'z') {
+        s[0] -= 32;
+    }
+
+    s[len] = '\0';
+
+    return s;
 }
 
 size_t Max_String_Length(char *strings[], size_t size) {
     size_t maxLength = 0;
-    for (int i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; i++) {
         size_t length = strlen(strings[i]);
         if (length > maxLength) {
             maxLength = length;
         }
     }
     return maxLength;
-}
-
-void cyan()
-{
-	printf("\033[1;96m");
-}
-void green()
-{
-	printf("\033[1;92m");
-}
-void red()
-{
-	printf("\033[1;91m");
-}
-void yellow()
-{
-	printf("\033[1;93m");
-}
-void reset()
-{
-	printf("\033[0;0m");
 }
--- lib.h
+++ lib.h
@@ -15,15 +15,31 @@
 
 typedef char * string;
 
+static inline void seed_random();
 int read_line(char str[], int n);
 int get_random_int(int lower_range, int upper_range);
 char* strLower(const char *str);
 char* strUpper(const char *str);
-char *capitalize(const char *str);
+char* capitalize(const char *str);
 size_t Max_String_Length(char *strings[], size_t size);
 
-void cyan();
-void green();
-void red();
-void yellow();
-void reset();
+
+static inline void cyan() {
+    printf("\033[1;96m");
+}
+
+static inline void green() {
+    printf("\033[1;92m");
+}
+
+static inline void red() {
+    printf("\033[1;91m");
+}
+
+static inline void yellow() {
+    printf("\033[1;93m");
+}
+
+static inline void reset() {
+    printf("\033[0;0m");
+}
