971b973d7c551e798a2be56dc1a6448ea1dd8e7b
AM-ML <aliredamoumneh@gmail.com>
2024-06-12 13:28:55 +0300

    + 14.5 _Pragma Macro
    _Pragma(x) -> #pragma #x

---
+++ rsc/14_5__Pragma_directive/c.c
@@ -0,0 +1,12 @@
+#include "lib.h"
+_Pragma("GCC diagnostic ignored \"-Wunused-variable\"")
+// same as writing #pragma GCC diagnostic ignored "-Wunused-variable"
+
+int main(int argc, char **argv)
+{
+	int i = 2;
+
+	PRINT_INT(i);
+
+    return 0;
+}
