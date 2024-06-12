7e5cb719c45714e01a8ec168a6c4af05697fbc58
AM-ML <aliredamoumneh@gmail.com>
2024-06-12 15:59:04 +0300

    + 15.2 Conditional File Inclusion
    #ifdef MACRO, #include ...

---
+++ rsc/15_2_conditional_file_inclusion/c.c
@@ -0,0 +1,18 @@
+#include "lib.h"
+
+#define INC 1
+
+// i call it conditional file inclusion :)
+
+#ifdef INC
+#include <time.h>
+#include <string.h>
+#include <sys/socket.h>
+#endif
+
+int main(int argc, char *argv[])
+{
+	
+
+	return 0;
+}
