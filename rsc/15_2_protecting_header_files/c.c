d106c7b71c7c7d3178c88f4f20c7dcd248767d15
AM-ML <aliredamoumneh@gmail.com>
2024-06-12 16:24:19 +0300

    15.2 protecting header files
    how to protect .h files from being included multiple times

---
+++ rsc/15_2_protecting_header_files/c.c
@@ -0,0 +1,13 @@
+// include all < > header files here to avoid multiple inclusions thus errors
+// e.g: f1.h includes f3.h, f2.h includes f3.h, and c.c includes f1.h and f2.h
+// including f3.h multiple times may cause errors.
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+	
+
+	return 0;
+}
---
+++ rsc/15_2_protecting_header_files/stack.h
@@ -0,0 +1,9 @@
+// *** Protects against multiple inclusions, ensures stack.h is included once ***
+
+#ifndef STACK_H
+#define STACK_H
+
+BOOL is_empty(void);
+BOOL is_full(void);
+
+#endif
