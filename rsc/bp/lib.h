c7904cf0110509e02ab15f371328b27c6ddea401
AM-ML <aliredamoumneh@gmail.com>
2024-06-24 16:33:24 +0300

    rsc/bp refactoring.

--- rsc/bp/lib.h
+++ rsc/bp/lib.h
@@ -7,12 +7,14 @@
 #define YELLOW "\033[1;93m"
 #define BLUE "\033[1;94m"
 
-#define PRINT_INT(n) printf("\033[1;92m::: " #n " \033[0;34m= \033[1;96m%d\n\033[0;0m", n)
-#define TEST(condition, ...) ((condition) ?\
-	printf("\033[1;92mPassed: \033[1;96m%s\033[0;0m\n", #condition) : \
-	printf(__VA_ARGS__))
+#define PRINT_INT(n)                                                           \
+  printf("\033[1;92m::: " #n " \033[0;34m= \033[1;96m%d\n\033[0;0m", n)
+#define TEST(condition, ...)                                                   \
+  ((condition)                                                                 \
+       ? printf("\033[1;92mPassed: \033[1;96m%s\033[0;0m\n", #condition)       \
+       : printf(__VA_ARGS__))
 
-typedef char * string;
+typedef char *string;
 
 #define cyan printf("\033[1;96m")
 #define yellow printf("\033[1;93m")
