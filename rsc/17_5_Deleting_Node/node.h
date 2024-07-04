f5ac01da16978c9308206f0e3f971b6df62721fd
AM-ML <aliredamoumneh@gmail.com>
2024-07-05 02:41:27 +0300

    Program Refactoring / Modification
    Change: fixed deletion causing error if node is root

--- rsc/17_5_Deleting_Node/node.h
+++ rsc/17_5_Deleting_Node/node.h
@@ -14,7 +14,7 @@ void add_node(Node **root, int number);
 void print_nodes(Node *root);
 Node *search_list(Node *root, int target);
 Node* PC_search(Node *root, int target, Node **p);
-void delete_node(Node *root, int target);
+void delete_node(Node **root, int target);
 
 Node *create_node(int number) {
   Node *node = (Node *)malloc(sizeof(Node));
@@ -65,11 +65,16 @@ Node* PC_search(Node *root, int target, Node **p) { //prev-cur search
   return cur;
 }
 
-void delete_node(Node *root, int target) {
+void delete_node(Node **root, int target) {
   Node *previous_node, *current_node;
 
-  current_node = PC_search(root, target, &previous_node);
-  previous_node -> next = current_node -> next;
+  current_node = PC_search(*root, target, &previous_node);
+
+  if (current_node == NULL) return; // node not found
+  if (previous_node == NULL) // node was root
+    *root = current_node -> next;
+  else
+    previous_node -> next = current_node -> next;
   free(current_node);
 }
 
